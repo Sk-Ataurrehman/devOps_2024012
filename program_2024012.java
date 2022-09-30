82db942c5ca3b91a46a297d7f37d70f3970b0ac8
Ataurrehman <shaikhataurrehman0@gmail.com>
2022-09-30 14:14:51 +0530

    first commit

--- program_2024012.java
+++ program_2024012.java
@@ -4,6 +4,7 @@ class program_2024012{
      int number=5;
      for(i=1;i<=number;i++){    
          fact=fact*i;    
+         
      }    
      System.out.println("Factorial of "+number+" is: "+fact);    
     }  
